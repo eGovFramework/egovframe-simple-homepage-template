8f0b122aceffafd76c9bebfa15099908bcec0974
이백행 <dlqorgod@naver.com>
2024-09-27 10:09:17 +0900

    Source > Format

--- src/main/java/egovframework/let/cop/com/web/EgovBBSUseInfoManageController.java
+++ src/main/java/egovframework/let/cop/com/web/EgovBBSUseInfoManageController.java
@@ -2,21 +2,13 @@ package egovframework.let.cop.com.web;
 
 import java.util.Map;
 
-import egovframework.com.cmm.EgovMessageSource;
-import egovframework.com.cmm.LoginVO;
-import egovframework.com.cmm.util.EgovUserDetailsHelper;
-import egovframework.let.cop.com.service.BoardUseInf;
-import egovframework.let.cop.com.service.BoardUseInfVO;
-import egovframework.let.cop.com.service.EgovBBSUseInfoManageService;
-
-import org.egovframe.rte.fdl.cmmn.exception.EgovBizException;
-import org.egovframe.rte.fdl.property.EgovPropertyService;
-import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
+import org.egovframe.rte.fdl.cmmn.exception.EgovBizException;
+import org.egovframe.rte.fdl.property.EgovPropertyService;
+import org.egovframe.rte.ptl.mvc.tags.ui.pagination.PaginationInfo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
@@ -26,6 +18,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.support.SessionStatus;
 import org.springmodules.validation.commons.DefaultBeanValidator;
+
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.cop.com.service.BoardUseInf;
+import egovframework.let.cop.com.service.BoardUseInfVO;
+import egovframework.let.cop.com.service.EgovBBSUseInfoManageService;
 //SHT-CUSTOMIZING//import egovframework.let.cop.clb.service.ClubUser;
 //SHT-CUSTOMIZING//import egovframework.let.cop.clb.service.EgovClubManageService;
 //SHT-CUSTOMIZING//import egovframework.let.cop.cmy.service.CommunityUser;
@@ -34,12 +33,13 @@ import org.springmodules.validation.commons.DefaultBeanValidator;
 
 /**
  * 게시판의 이용정보를 관리하기 위한 컨트롤러 클래스
+ * 
  * @author 공통서비스개발팀 이삼섭
  * @since 2009.04.02
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *   수정일      수정자           수정내용
@@ -47,106 +47,110 @@ import org.springmodules.validation.commons.DefaultBeanValidator;
  *   2009.04.02  이삼섭          최초 생성
  *   2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
- * </pre>
+ *      </pre>
  */
 @Controller
 public class EgovBBSUseInfoManageController {
 
 	/** EgovBBSUseInfoManageService */
-    @Resource(name = "EgovBBSUseInfoManageService")
-    private EgovBBSUseInfoManageService bbsUseService;
+	@Resource(name = "EgovBBSUseInfoManageService")
+	private EgovBBSUseInfoManageService bbsUseService;
 
-    /** EgovPropertyService */
-    @Resource(name = "propertiesService")
-    protected EgovPropertyService propertyService;
+	/** EgovPropertyService */
+	@Resource(name = "propertiesService")
+	protected EgovPropertyService propertyService;
 
-    /** DefaultBeanValidator */
-    @Autowired
-    private DefaultBeanValidator beanValidator;
+	/** DefaultBeanValidator */
+	@Autowired
+	private DefaultBeanValidator beanValidator;
 
-    /** EgovMessageSource */
-    @Resource(name="egovMessageSource")
-    EgovMessageSource egovMessageSource;
+	/** EgovMessageSource */
+	@Resource(name = "egovMessageSource")
+	EgovMessageSource egovMessageSource;
 
-    /**
-     * 게시판 사용 정보를 삭제한다.
-     *
-     * @param bdUseVO
-     * @param bdUseInf
-     * @param sessionVO
-     * @param status
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/deleteBBSUseInf.do")
-    public String deleteBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, @ModelAttribute("bdUseInf") BoardUseInf bdUseInf,
-	    SessionStatus status, ModelMap model) throws Exception {
+	/**
+	 * 게시판 사용 정보를 삭제한다.
+	 *
+	 * @param bdUseVO
+	 * @param bdUseInf
+	 * @param sessionVO
+	 * @param status
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/deleteBBSUseInf.do")
+	public String deleteBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO,
+			@ModelAttribute("bdUseInf") BoardUseInf bdUseInf, SessionStatus status, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
 		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
 		if (isAuthenticated) {
-		    bbsUseService.deleteBBSUseInf(bdUseInf);
+			bbsUseService.deleteBBSUseInf(bdUseInf);
 		}
 
 		return "forward:/cop/com/selectBBSUseInfs.do";
-    }
+	}
 
-    /**
-     * 게사판 사용정보 등록을 위한 등록페이지로 이동한다.
-     *
-     * @param bdUseVO
-     * @param sessionVO
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/addBBSUseInf.do")
-    public String addBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
+	/**
+	 * 게사판 사용정보 등록을 위한 등록페이지로 이동한다.
+	 *
+	 * @param bdUseVO
+	 * @param sessionVO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/addBBSUseInf.do")
+	public String addBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
-    	return "cop/com/EgovBoardUseInfRegist";
-    }
+		return "cop/com/EgovBoardUseInfRegist";
+	}
 
-    /**
-     * 게시판 사용정보를 등록한다.
-     *
-     * @param bdUseVO
-     * @param bdUseInf
-     * @param sessionVO
-     * @param status
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/insertBBSUseInf.do")
-    public String insertBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, @ModelAttribute("boardUseInf") BoardUseInf boardUseInf,
-	    BindingResult bindingResult, @RequestParam Map<String, Object> commandMap, ModelMap model) throws Exception {
+	/**
+	 * 게시판 사용정보를 등록한다.
+	 *
+	 * @param bdUseVO
+	 * @param bdUseInf
+	 * @param sessionVO
+	 * @param status
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/insertBBSUseInf.do")
+	public String insertBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO,
+			@ModelAttribute("boardUseInf") BoardUseInf boardUseInf, BindingResult bindingResult,
+			@RequestParam Map<String, Object> commandMap, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
-	    LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
-	    Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
+		LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
+		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
 		beanValidator.validate(boardUseInf, bindingResult);
 
 		if (bindingResult.hasErrors()) {
-		    return "cop/com/EgovBoardUseInfRegist";
+			return "cop/com/EgovBoardUseInfRegist";
 		}
 
-		String trgetType = (String)commandMap.get("param_trgetType");
+		String trgetType = (String) commandMap.get("param_trgetType");
 		String registSeCode = "";
 
 		// CMMNTY 06/CLUB 05/SYSTEM(REGC01)
 		if ("CMMNTY".equals(trgetType)) {
-		    registSeCode = "REGC06";
+			registSeCode = "REGC06";
 		} else if ("CLUB".equals(trgetType)) {
-		    registSeCode = "REGC05";
+			registSeCode = "REGC05";
 		} else {
-		    registSeCode = "REGC01";
+			registSeCode = "REGC01";
 		}
 
 		boardUseInf.setUseAt("Y");
@@ -154,33 +158,32 @@ public class EgovBBSUseInfoManageController {
 		boardUseInf.setRegistSeCode(registSeCode);
 
 		if (isAuthenticated) {
-		    bbsUseService.insertBBSUseInf(boardUseInf);
+			bbsUseService.insertBBSUseInf(boardUseInf);
 		}
 
 		return "forward:/cop/com/selectBBSUseInfs.do";
-    }
+	}
 
-    /**
-     * 게시판 사용정보 목록을 조회한다.
-     *
-     * @param bdUseVO
-     * @param sessionVO
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/selectBBSUseInfs.do")
-    public String selectBBSUseInfs(HttpSession session, 
-			@RequestParam(value="menuNo", required=false) String menuNo,
-    		@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, 
-    		ModelMap model) throws Exception {
-    	
-    	// 선택된 메뉴정보를 세션으로 등록한다.
-    	if (menuNo!=null && !menuNo.equals("")){
-    		session.setAttribute("menuNo",menuNo);
-    	}
+	/**
+	 * 게시판 사용정보 목록을 조회한다.
+	 *
+	 * @param bdUseVO
+	 * @param sessionVO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/selectBBSUseInfs.do")
+	public String selectBBSUseInfs(HttpSession session, @RequestParam(value = "menuNo", required = false) String menuNo,
+			@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		// 선택된 메뉴정보를 세션으로 등록한다.
+		if (menuNo != null && !menuNo.equals("")) {
+			session.setAttribute("menuNo", menuNo);
+		}
+
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
 		bdUseVO.setPageUnit(propertyService.getInt("pageUnit"));
 		bdUseVO.setPageSize(propertyService.getInt("pageSize"));
@@ -196,7 +199,7 @@ public class EgovBBSUseInfoManageController {
 		bdUseVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
 
 		Map<String, Object> map = bbsUseService.selectBBSUseInfs(bdUseVO);
-		int totCnt = Integer.parseInt((String)map.get("resultCnt"));
+		int totCnt = Integer.parseInt((String) map.get("resultCnt"));
 
 		paginationInfo.setTotalRecordCount(totCnt);
 
@@ -205,74 +208,79 @@ public class EgovBBSUseInfoManageController {
 		model.addAttribute("paginationInfo", paginationInfo);
 
 		return "cop/com/EgovBoardUseInfList";
-    }
+	}
 
-    /**
-     * 게시판 사용정보를 수정한다.
-     *
-     * @param bdUseVO
-     * @param bdUseInf
-     * @param sessionVO
-     * @param status
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/updateBBSUseInf.do")
-    public String updateBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, @ModelAttribute("boardUseInf") BoardUseInf boardUseInf,
-	    HttpServletRequest request, ModelMap model) throws Exception {
+	/**
+	 * 게시판 사용정보를 수정한다.
+	 *
+	 * @param bdUseVO
+	 * @param bdUseInf
+	 * @param sessionVO
+	 * @param status
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/updateBBSUseInf.do")
+	public String updateBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO,
+			@ModelAttribute("boardUseInf") BoardUseInf boardUseInf, HttpServletRequest request, ModelMap model)
+			throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
-    	Boolean isAuthenticated = (LoginVO)request.getSession().getAttribute("LoginVO") == null ? false:true;
+		Boolean isAuthenticated = (LoginVO) request.getSession().getAttribute("LoginVO") == null ? false : true;
 
 		if (isAuthenticated) {
-		    bbsUseService.updateBBSUseInf(boardUseInf);
+			bbsUseService.updateBBSUseInf(boardUseInf);
 		}
 
 		return "forward:/cop/com/selectBBSUseInfs.do";
-    }
+	}
 
-    /**
-     * 게시판 사용정보에 대한 상세정보를 조회한다.
-     *
-     * @param bdUseVO
-     * @param sessionVO
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/selectBBSUseInf.do")
-    public String selectBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
-	BoardUseInfVO vo = bbsUseService.selectBBSUseInf(bdUseVO);
+	/**
+	 * 게시판 사용정보에 대한 상세정보를 조회한다.
+	 *
+	 * @param bdUseVO
+	 * @param sessionVO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/selectBBSUseInf.do")
+	public String selectBBSUseInf(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
+		BoardUseInfVO vo = bbsUseService.selectBBSUseInf(bdUseVO);
 
-	    if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
 		// 시스템 사용 게시판의 경우 URL 표시
 		if ("SYSTEM_DEFAULT_BOARD".equals(vo.getTrgetId())) {
-		    if (vo.getBbsTyCode().equals("BBST02")) {	// 익명게시판
-		    	vo.setProvdUrl("/cop/bbs/anonymous/selectBoardList.do?bbsId=" + vo.getBbsId());
-		    } else {
-		    	vo.setProvdUrl("/cop/bbs/selectBoardList.do?bbsId=" + vo.getBbsId());
-		    }
+			if (vo.getBbsTyCode().equals("BBST02")) { // 익명게시판
+				vo.setProvdUrl("/cop/bbs/anonymous/selectBoardList.do?bbsId=" + vo.getBbsId());
+			} else {
+				vo.setProvdUrl("/cop/bbs/selectBoardList.do?bbsId=" + vo.getBbsId());
+			}
 		}
 
 		model.addAttribute("bdUseVO", vo);
 		return "cop/com/EgovBoardUseInfInqire";
-    }
+	}
 
-    /**
-     * 커뮤니티, 동호회에 사용되는 게시판 사용정보에 대한 목록을 조회한다.
-     *
-     * @param bdUseVO
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/selectBBSUseInfsByTrget.do")
-    public String selectBBSUseInfsByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model) throws Exception {
+	/**
+	 * 커뮤니티, 동호회에 사용되는 게시판 사용정보에 대한 목록을 조회한다.
+	 *
+	 * @param bdUseVO
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/selectBBSUseInfsByTrget.do")
+	public String selectBBSUseInfsByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, ModelMap model)
+			throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
 		bdUseVO.setPageUnit(propertyService.getInt("pageUnit"));
 		bdUseVO.setPageSize(propertyService.getInt("pageSize"));
@@ -288,7 +296,7 @@ public class EgovBBSUseInfoManageController {
 		bdUseVO.setRecordCountPerPage(paginationInfo.getRecordCountPerPage());
 
 		Map<String, Object> map = bbsUseService.selectBBSUseInfsByTrget(bdUseVO);
-		int totCnt = Integer.parseInt((String)map.get("resultCnt"));
+		int totCnt = Integer.parseInt((String) map.get("resultCnt"));
 
 		paginationInfo.setTotalRecordCount(totCnt);
 
@@ -299,84 +307,88 @@ public class EgovBBSUseInfoManageController {
 		model.addAttribute("paginationInfo", paginationInfo);
 
 		return "cop/com/EgovBdUseInfListByTrget";
-    }
+	}
 
-    /**
-     * 커뮤니티, 동호회에 사용되는 게시판 사용정보를 수정한다.
-     *
-     * @param bdUseVO
-     * @param boardUseInf
-     * @param status
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/updateBBSUseInfByTrget.do")
-    public String updateBBSUseInfByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, @ModelAttribute("boardUseInf") BoardUseInf boardUseInf,
-    		@RequestParam Map<String, Object> commandMap, SessionStatus status, ModelMap model) throws Exception {
+	/**
+	 * 커뮤니티, 동호회에 사용되는 게시판 사용정보를 수정한다.
+	 *
+	 * @param bdUseVO
+	 * @param boardUseInf
+	 * @param status
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/updateBBSUseInfByTrget.do")
+	public String updateBBSUseInfByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO,
+			@ModelAttribute("boardUseInf") BoardUseInf boardUseInf, @RequestParam Map<String, Object> commandMap,
+			SessionStatus status, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
-		String param_trgetId = (String)commandMap.get("param_trgetId");
+		String param_trgetId = (String) commandMap.get("param_trgetId");
 
 		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
 		if (isAuthenticated) {
-		    boardUseInf.setTrgetId(param_trgetId);
-		    bbsUseService.updateBBSUseInfByTrget(boardUseInf);
+			boardUseInf.setTrgetId(param_trgetId);
+			bbsUseService.updateBBSUseInfByTrget(boardUseInf);
 		}
 
 		return "forward:/cop/com/selectBBSUseInfsByTrget.do";
-    }
+	}
 
-    /**
-     * 커뮤니티, 동호회에 사용되는 게시판 사용정보를 등록한다.
-     *
-     * @param bdUseVO
-     * @param boardUseInf
-     * @param status
-     * @param model
-     * @return
-     * @throws Exception
-     */
-    @RequestMapping("/cop/com/insertBBSUseInfByTrget.do")
-    public String insertBBSUseInfByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO, @ModelAttribute("boardUseInf") BoardUseInf boardUseInf,
-    		@RequestParam Map<String, Object> commandMap, SessionStatus status, ModelMap model) throws Exception {
+	/**
+	 * 커뮤니티, 동호회에 사용되는 게시판 사용정보를 등록한다.
+	 *
+	 * @param bdUseVO
+	 * @param boardUseInf
+	 * @param status
+	 * @param model
+	 * @return
+	 * @throws Exception
+	 */
+	@RequestMapping("/cop/com/insertBBSUseInfByTrget.do")
+	public String insertBBSUseInfByTrget(@ModelAttribute("searchVO") BoardUseInfVO bdUseVO,
+			@ModelAttribute("boardUseInf") BoardUseInf boardUseInf, @RequestParam Map<String, Object> commandMap,
+			SessionStatus status, ModelMap model) throws Exception {
 
-    	if (!checkAuthority(model)) return "cmm/uat/uia/EgovLoginUsr";	// server-side 권한 확인
+		if (!checkAuthority(model))
+			return "cmm/uat/uia/EgovLoginUsr"; // server-side 권한 확인
 
-		String paramTrgetId = (String)commandMap.get("param_trgetId");
-		String bbsId = (String)commandMap.get("bbsId");
+		String paramTrgetId = (String) commandMap.get("param_trgetId");
+		String bbsId = (String) commandMap.get("bbsId");
 
-		LoginVO user = (LoginVO)EgovUserDetailsHelper.getAuthenticatedUser();
+		LoginVO user = (LoginVO) EgovUserDetailsHelper.getAuthenticatedUser();
 		Boolean isAuthenticated = EgovUserDetailsHelper.isAuthenticated();
 
 		if (isAuthenticated) {
-		    boardUseInf.setUseAt("Y");
-		    boardUseInf.setFrstRegisterId(user.getUniqId());
-		    boardUseInf.setRegistSeCode("REGC07");
-		    boardUseInf.setBbsId(bbsId);
-		    boardUseInf.setTrgetId(paramTrgetId);
+			boardUseInf.setUseAt("Y");
+			boardUseInf.setFrstRegisterId(user.getUniqId());
+			boardUseInf.setRegistSeCode("REGC07");
+			boardUseInf.setBbsId(bbsId);
+			boardUseInf.setTrgetId(paramTrgetId);
 
-		    bbsUseService.insertBBSUseInf(boardUseInf);
+			bbsUseService.insertBBSUseInf(boardUseInf);
 		}
 
 		return "forward:/cop/com/selectBBSUseInfsByTrget.do";
-    }
+	}
 
-    /**
-     * 운영자 권한을 확인한다.(로그인 여부를 확인한다.)
-     *
-     * @param boardMaster
-     * @throws EgovBizException
-     */
-    protected boolean checkAuthority(ModelMap model) throws Exception {
-    	// 사용자권한 처리
-    	if(!EgovUserDetailsHelper.isAuthenticated()) {
-    		model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
-        	return false;
-    	}else{
-    		return true;
-    	}
-    }
+	/**
+	 * 운영자 권한을 확인한다.(로그인 여부를 확인한다.)
+	 *
+	 * @param boardMaster
+	 * @throws EgovBizException
+	 */
+	protected boolean checkAuthority(ModelMap model) throws Exception {
+		// 사용자권한 처리
+		if (!EgovUserDetailsHelper.isAuthenticated()) {
+			model.addAttribute("message", egovMessageSource.getMessage("fail.common.login"));
+			return false;
+		} else {
+			return true;
+		}
+	}
 }
