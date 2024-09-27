6146b18ca6458dedbde2545e5264ead922a0cfb8
이백행 <bhlee@gsitm.com>
2024-09-27 16:52:20 +0900

    Source > Format

--- src/main/java/egovframework/let/uat/uia/web/EgovLoginController.java
+++ src/main/java/egovframework/let/uat/uia/web/EgovLoginController.java
@@ -1,17 +1,11 @@
 package egovframework.let.uat.uia.web;
 
-import egovframework.com.cmm.EgovMessageSource;
-import egovframework.com.cmm.LoginVO;
-import egovframework.com.cmm.util.EgovUserDetailsHelper;
-import egovframework.let.uat.uia.service.EgovLoginService;
-
-import org.egovframe.rte.fdl.cmmn.trace.LeaveaTrace;
-import org.egovframe.rte.fdl.property.EgovPropertyService;
-
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.egovframe.rte.fdl.cmmn.trace.LeaveaTrace;
+import org.egovframe.rte.fdl.property.EgovPropertyService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,14 +13,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 
+import egovframework.com.cmm.EgovMessageSource;
+import egovframework.com.cmm.LoginVO;
+import egovframework.com.cmm.util.EgovUserDetailsHelper;
+import egovframework.let.uat.uia.service.EgovLoginService;
+
 /**
  * 일반 로그인을 처리하는 컨트롤러 클래스
+ * 
  * @author 공통서비스 개발팀 박지욱
  * @since 2009.03.06
  * @version 1.0
  * @see
  *
- * <pre>
+ *      <pre>
  * << 개정이력(Modification Information) >>
  *
  *  수정일      수정자      수정내용
@@ -34,7 +34,7 @@ import org.springframework.web.context.request.RequestContextHolder;
  *  2009.03.06  박지욱     최초 생성
  *  2011.08.31  JJY            경량환경 템플릿 커스터마이징버전 생성
  *
- *  </pre>
+ *      </pre>
  */
 @Controller
 public class EgovLoginController {
@@ -57,24 +57,28 @@ public class EgovLoginController {
 
 	/**
 	 * 로그인 화면으로 들어간다
+	 * 
 	 * @param vo - 로그인후 이동할 URL이 담긴 LoginVO
 	 * @return 로그인 페이지
 	 * @exception Exception
 	 */
 	@RequestMapping(value = "/uat/uia/egovLoginUsr.do")
-	public String loginUsrView(@ModelAttribute("loginVO") LoginVO loginVO, HttpServletRequest request, HttpServletResponse response, ModelMap model) throws Exception {
+	public String loginUsrView(@ModelAttribute("loginVO") LoginVO loginVO, HttpServletRequest request,
+			HttpServletResponse response, ModelMap model) throws Exception {
 		return "cmm/uat/uia/EgovLoginUsr";
 	}
 
 	/**
 	 * 일반 로그인을 처리한다
-	 * @param vo - 아이디, 비밀번호가 담긴 LoginVO
+	 * 
+	 * @param vo      - 아이디, 비밀번호가 담긴 LoginVO
 	 * @param request - 세션처리를 위한 HttpServletRequest
 	 * @return result - 로그인결과(세션정보)
 	 * @exception Exception
 	 */
 	@RequestMapping(value = "/uat/uia/actionLogin.do")
-	public String actionLogin(@ModelAttribute("loginVO") LoginVO loginVO, HttpServletRequest request, ModelMap model) throws Exception {
+	public String actionLogin(@ModelAttribute("loginVO") LoginVO loginVO, HttpServletRequest request, ModelMap model)
+			throws Exception {
 
 		// 1. 일반 로그인 처리
 		LoginVO resultVO = loginService.actionLogin(loginVO);
@@ -95,6 +99,7 @@ public class EgovLoginController {
 
 	/**
 	 * 로그인 후 메인화면으로 들어간다
+	 * 
 	 * @param
 	 * @return 로그인 페이지
 	 * @exception Exception
@@ -115,6 +120,7 @@ public class EgovLoginController {
 
 	/**
 	 * 로그아웃한다.
+	 * 
 	 * @return String
 	 * @exception Exception
 	 */
